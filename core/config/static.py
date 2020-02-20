9e3f1a033006a20764f851aeaef313629cba63d4
udayaks <30748755+udayaks@users.noreply.github.com>
2020-02-21 00:19:30 +0545

    Add files via upload

---
+++ core/config/static.py
@@ -0,0 +1,33 @@
+
+pattern_remove_desc = "(hotel|resort|repair|installation|HVAC|solutions|firm|dealership|blog|IT.Service|" \
+                      "banking|bank|finance|currency|consulting|web.development|SEO|advertisement|" \
+                      "project.management|management|certification|cruise|vacation|coupons|promos|" \
+                      "lawyer|law.firm|injury|insurance|risk|dentist|Dr.MD|staffing|human.resource|" \
+                      "associates|news|dictionary|locksmith|enforcement|how.to|tutorial|review|auto.repair|" \
+                      "top.10|CLOSED|nightclub|contractor|industrial|license|exam|association|prep.school|" \
+                      "roofing|GmbH|masonry|free.estimate|estimate|quote|flooring|architecture|engineering|" \
+                      "comapany.profile|research|rental|Mp3|software|PPC|adwords|salon|marketing|analytics|" \
+                      "optometrist|conferencing|agency|development|cloud|sightseeing|tours|package|real.estate|" \
+                      "home.buying|wordpress|airport|parking|diagnostic|consumer|mechanical|milling|classified|" \
+                      "logistics|helicopter|charter|university|loan|trading|free.games|salesforce|outlook|" \
+                      "website.information|apartment|bunglow|time.share|radio|FM|streaming|company.information|" \
+                      "expert.service|magazine|virtual|Milling|whois|brewery|vineyard|greenhouses|committee|" \
+                      "Internet.Service|community|airlines|domain|android|Bureau®|call.forwarding|Financial|" \
+                      "campaign|salvage|learning|accounting|landscape|landscaping|lawn.care|pesticide|insecticide|" \
+                      "city.of|district.of|leasing|shopping.centre|town.of|brewing|dispensary|transportation|" \
+                      "freight|cargo|cutestat\.com|clinic|hospital|hospitality|401K|portfolio|1040|1099|web.design|" \
+                      "leadership|institute|solution|VoIP|advisor|academy|driving.school|loft|funding|screen.printing|" \
+                      "granite|relocation|powerpoint|event.production|recording.studio|studio|mortgage|conference|concert|" \
+                      "tickets|trade.show|seminar|transcription|medical.billing|medical.coding|freelance|IT.Support|electrician|" \
+                      "knowledgebase|venture.capital|mutual.fund|asset.management|portfolio.management|risk.management|photographer|" \
+                      "credit.card|credit.report|florist|spa.care|IIS7|website.informer|DJ.service|pest.removal|restuarant.delivery|" \
+                      "food.delivery|floral.delivery|financial|consultancy)"
+pattern_remove_url = "\.facebook\.|\.instagram\.|\.bhphoto\.|\.amazon\.|\.youtube\.|\.bloomberg\.|\.wikipedia\.|" \
+                     "\.pinterest\.|\.zomato\.|\.walmart\.|\.bbb.org\.|\.mapquest\.|\.walgreens\.|\.buybuy\.|\.yellowpages\.|" \
+                     "\.petco\.|\.nordstrom\.|\.yp\.|\.petsmart\.|\.advanceautoparts\.|\.wayfair\.|\.chewy\.|\.oreilly\.|\.dictionary\.|" \
+                     "\.mercari\.|\.poshmark\.|\.thesaurus\.|\.bestbuy\.|\.dillards\.|\.merriam-webster\.|\.rakuten\.|\.IMDB\.|\.edu\.|" \
+                     "\.linkedin\.|\.Macys\.|\.gov\.|\.kohls\.|\.google\.|\.bizjournals\.|\.urbandictionary\.|\.ebay\.|\.webmd\.com|" \
+                     "\.ecmag\.com|\.inc\.com|\.bustle\.com|\.astrology-zodiac-signs\.com|\.cutestat.\.com|.poshmark.|.buckle\.com|.tumblr\.com"
+#___for language to be excluded, these are language determined by 'langdetect' module
+pattern_remove_lan = "(bg|pl|vi|ko|ja|fa|fr|hr|sv|th|de|zh-cn|ru|nl|tr|zh-tw|it|he|es|ar|pt|zh-tw|tr|id|ca|zh)"
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from core.regexsearch import Regex_Search
+
+if __name__=="__main__":
+    Regex_Search().start()
